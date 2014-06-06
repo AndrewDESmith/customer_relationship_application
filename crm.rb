b4a218a9855bfd0bc5d238125185e8113ed3c724
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-06-06 11:29:13 -0400

    2nd Revision of CRM.

--- crm.rb
+++ crm.rb
@@ -1,18 +1,55 @@
-def print_main_menu
-  puts "[1] Add a new contact"
-  puts "[2] Modify an existing contact"
-  puts "[3] Delete a contact"
-  puts "[4] Display all the contacts"
-  puts "[5] Display an attribute"
-  puts "[6] Exit"
-  puts "Enter a number: "
+class CRM
+	attr_accesor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+	def print_main_menu
+	  puts "[1] Add a new contact"
+	  puts "[2] Modify an existing contact"
+	  puts "[3] Delete a contact"
+	  puts "[4] Display all the contacts"
+	  puts "[5] Display an attribute"
+	  puts "[6] Exit"
+	  puts "Enter a number: "
+	end
+
+	def main_menu
+		puts "Welcome to #{@name}"
+		# Have a getter method for name (since in attr_accessor : name),
+		# so the @ in front of name is optional.
+		print_main_menu
+		selection = gets.chomp.to_i
+	  call_option(selection)
+	end
+
+	def call_option(selection)
+		case selection
+		when 1 then add_new_contact
+		when 2 then modify_contact
+		when 3 then delete_contact
+		when 4 then display_contact
+		when 5 then display_attribute
+		when 6 
+			puts "Goodbye!"
+			return
+		else
+			puts "Invalid input.  Please enter a number from 1 to 6."
+			main_menu
+		end
+	end
 end
 
-def main_menu
-	print_main_menu
-	selection = gets.chomp.to_i
-  # call_option(selection)
-end
+crm = CRM.new("Bitmaker Labs CRM")
+crm.main_menu
 
+# class Contact
+# 	attr_accessor :id, :first_name, :last_name, :email, :notes
 
-main_menu
+# 	def initialize(id, first_name, last_name, email, notes)
+#			@id = id
+# 	end
+# end
+
+# contact = Contact.new(id, first_name, last_name, email, notes)
